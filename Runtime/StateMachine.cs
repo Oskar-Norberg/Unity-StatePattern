1226c7e5c04095239ee67bfff5ebfd378a033a37
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-13 18:37:03 +0100

    EDIT: [Script] Utilize Dictionary to store states

--- Runtime/StateMachine.cs
+++ Runtime/StateMachine.cs
@@ -1,38 +1,42 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace StatePattern
 {
+    /**
+     * <summary>
+     * Simple State Machine.
+     * To tick behaviour tree call the UpdateState and FixedUpdateState methods in the derived class.
+     * </summary>
+     */
     public abstract class StateMachine : MonoBehaviour
     {
         [SerializeField] private State startState;
-        [SerializeField] private List<State> states = new();
+        [SerializeField] private State[] states;
+        
+        private Dictionary<Type, State> _stateDictionary = new();
     
         protected State currentState;
 
         protected void Awake()
         {
+            InitializeStateDictionary(states);
+            
             SwitchState(startState);
         }
 
         public void SwitchState<TNextState>()
         {
-            foreach (State state in states)
+            if (_stateDictionary.TryGetValue(typeof(TNextState), out State state))
             {
-                if (state.GetType() != typeof(TNextState)) continue;
-            
                 SwitchState(state);
                 return;
             }
         
             Debug.Log("State not found");
         }
-    
-        private void SwitchState(State state)
         {
-            currentState?.ExitState();
-            currentState = state;
-            state.EnterState(this);
         }
 
         protected virtual void UpdateState()
@@ -43,6 +47,21 @@ namespace StatePattern
         protected virtual void FixedUpdateState()
         {
             currentState?.FixedUpdateState(Time.fixedDeltaTime);
+        }
+    
+        private void SwitchState(State state)
+        {
+            currentState?.ExitState();
+            currentState = state;
+            state.EnterState(this);
+        }
+        
+        private void InitializeStateDictionary(State[] states)
+        {
+            foreach (State state in states)
+            {
+                _stateDictionary.Add(state.GetType(), state);
+            }
         }
     }
 }
